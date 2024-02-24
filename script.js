bdbcf4fea1425c42604089e618512964d4907592
vaibhavyxx <vd5443@rit.edu>
2024-02-24 14:53:08 -0500

    datatype

---
+++ script.js
@@ -0,0 +1,23 @@
+const first ="sara";
+console.log(first, typeof first);
+let output; 
+const nums =[1,4,19];
+output=nums;
+//number
+const age=90;
+console.log(age, typeof age);
+
+const person={
+    name:'Brad',
+    age:'30'
+};
+
+function sayHello(){
+    console.log('hello world');
+}
+
+output = sayHello;
+
+const id=Symbol('id');
+const n = 9999999999999999999999n;
+console.log(output, typeof output);
