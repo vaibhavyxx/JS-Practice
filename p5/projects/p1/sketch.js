705d193e7ca458d30ab8ecd9a52c12c8bca91c10
vaibhavyxx <vd5443@rit.edu>
2024-06-19 17:00:31 +0530

    added switch

--- p5/projects/p1/sketch.js
+++ p5/projects/p1/sketch.js
@@ -10,9 +10,8 @@ scenes.
 7. Use p5 only for shapes and texts.
  */
 
-let toSwitch;
+let buttonClicked = undefined;
 function setup(){
-  toSwitch = -1;
     createCanvas(1080,1080);
     background(192, 210, 252);
     let button = createButton('click me');
@@ -20,16 +19,19 @@ function setup(){
 }
 
 function mousePressed(){
-  toSwitch *= -1;
+  if(!buttonClicked)
+    buttonClicked = true;
+  else
+  buttonClicked = false;
 }
 
 
 function draw(){
- 
-  if(toSwitch == -1){
-    //fish
-//body
-noStroke();
+
+if(buttonClicked){
+  background(192, 210, 252);
+  //show tuna
+  noStroke();
 fill(255);
 beginShape();
 vertex(295.5, 347);
@@ -181,7 +183,88 @@ vertex(452.5, 443);
 vertex(488.22, 408.53);
 endShape();
   }
-else{
-  
-}
+  else if(!buttonClicked){
+    background(192, 210, 252);
+    //green turtle
+    fill(255)
+    //body
+    noStroke();
+    bezier(340, 395, 359.98, 348.37, 407.5, 304.5, 459, 280.5);
+    bezier(459, 280.5, 494.5, 262, 497.89, 277.5, 509.5, 277.5)
+    bezier(509.5, 277.5, 555.5, 300, 608.5, 322.67, 608.5, 379);
+    bezier(608.5, 379, 608.5, 435.33, 558, 457.5, 491.5, 465.5);
+    bezier(491.5, 465.5, 425.78, 465.5, 321, 482.33, 321, 426);
+    bezier(321, 426, 321.64, 415.16, 333.59, 407.12, 340, 395);
+
+    beginShape()
+    vertex(340, 395)
+    vertex(459, 280.5)
+    vertex(509.5, 277.5)
+    vertex(608.5, 379)
+    vertex(491.5, 465.5)
+    vertex(321, 426)
+    endShape();
+
+    //tummy
+    fill(220);
+    bezier(398.5, 465, 384.21, 456.75, 417, 428.5, 417, 416.5)
+    bezier(417, 416.5, 417, 390.82, 456.85, 370, 506, 370)
+    bezier(506, 370, 555.15, 370, 595, 390.82, 595, 416.5)
+    bezier(595, 416.5, 595, 442.18, 534, 460.49, 506, 463)
+    bezier(506, 463, 472.5, 466, 414.79, 474.4, 398.5, 465)
+    beginShape();
+    vertex(398.5, 465)
+    vertex(417, 416.5)
+    vertex(506, 370)
+    vertex(595, 416.5)
+    vertex(506, 463)
+    endShape();
+
+    //left arm
+    fill(240)
+    bezier(218, 594.5, 271.5, 534, 385.38, 352.5, 421, 352.5)
+    bezier(421, 352.5, 456.62, 352.5, 390, 494.5, 360.5, 540.5)
+    bezier(360.5, 540.5, 319.8, 603.97, 274.12, 628.5, 238.5, 628.5)
+    bezier(238.5, 628.5, 202.88, 628.5, 199.5, 621, 218, 594.5)
+    beginShape()
+    vertex(218, 594.5)
+    vertex(421, 352.5)
+    vertex(360.5, 540.5)
+    vertex(238.5, 628.5)
+    endShape()
+
+    //right arm
+    bezier(582.88, 381.5, 582.88, 367.42, 584.5, 353.5, 602.5, 353.5)
+    bezier(602.5, 353.5, 617.14, 353.5, 642.5, 366.5, 658.5, 388)
+    bezier(658.5, 388, 675.54, 410.89, 706.17, 455.5, 725.5, 473.5)
+    bezier(725.5, 473.5, 744.72, 491.39, 751.5, 507, 767.5, 523.5)
+    bezier(767.5, 523.5, 796, 537.09, 774.85, 543.57, 761, 537.09)
+    bezier(761, 537.09, 754.96, 534.26, 746.4, 528.77, 735, 520)
+    bezier(735, 520, 723, 510.77, 661, 439.5, 649, 423)
+    bezier(649, 423, 637, 409.5, 619.03, 394, 612, 394)
+    bezier(612, 394, 597.36, 394, 582.88, 395.58, 582.88, 381.5)
+    beginShape()
+    vertex(582.88, 381.5)
+    vertex(602.5, 353.5)
+    vertex(658.5, 388)
+    vertex(725.5, 473.5)
+    vertex(767.5, 523.5)
+    vertex(761, 537.09)
+    vertex(735, 520)
+    vertex(649, 423)
+    vertex(612, 394)
+    endShape()
+
+    //head
+    bezier(508, 253, 542, 254.5, 554, 272.13, 554, 294.5)
+    bezier(554, 294.5, 555, 328.5, 526.35, 327, 501.5, 327)
+    bezier(501.5, 327, 476.65, 327, 469, 308.37, 469, 286)
+    bezier(469, 286, 469, 263.63, 478.5, 254, 508, 253)
+    beginShape()
+    vertex(508, 253)
+    vertex(554, 294.5)
+    vertex(501.5, 327)
+    vertex(469, 286)
+    endShape()
+  }
 }
